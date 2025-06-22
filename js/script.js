53de8bc611e46ac0988d6b19cbcb14fdf06a5300
Denen <awardenen@gmail.com>
2025-06-22 05:07:27 +0100

    Completed

--- js/script.js
+++ js/script.js
@@ -23,6 +23,27 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
+// const content = document.querySelector('.faq .content');
+
+// function expand() {
+//     const sectionHeight = content.scrollHeight;
+//     content.style.height = sectionHeight + 'px';
+
+//     // After the transition ends, set to auto for responsiveness
+//     content.addEventListener('transitionend', () => {
+//         content.style.height = 'auto';
+//     }, { once: true });
+// }
+
+// function collapse() {
+//     const sectionHeight = content.scrollHeight;
+//     content.style.height = sectionHeight + 'px';
+
+//     requestAnimationFrame(() => {
+//         content.style.height = '0';
+//     });
+// }
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -31,24 +52,65 @@ for (i = 0; i < coll.length; i++) {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
         var indicator = this.querySelector('i');
-        if (content.style.display === "block") {
-            content.style.display = "none";
+        if (content.style.height !== "0px") {
+            content.style.height = "0";
+            content.style.paddingTop = "0";
+            content.style.paddingBottom = "0";
             indicator.classList.remove('fa-minus');
             indicator.classList.add('fa-plus');
+
+            collapse();
         } else {
-            content.style.display = "block";
+            content.style.height = "auto";
+            content.style.paddingTop = "10px";
+            content.style.paddingBottom = "10px";
             indicator.classList.remove('fa-plus');
             indicator.classList.add('fa-minus');
+
+            expand();
         }
+
+
+
     });
 }
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "300px";
     document.body.classList.add("no-scroll");
+    var icon = document.querySelector('.sidebar-toggle i');
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-xmark');
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.classList.remove("no-scroll");
+    var icon = document.querySelector('.sidebar-toggle i');
+    icon.classList.remove('fa-xmark');
+    icon.classList.add('fa-bars');
+
 }
+
+function toggleNav() {
+    var sidenav = document.getElementById("mySidenav");
+    if (sidenav.style.width === "300px") {
+        closeNav();
+    } else {
+        openNav();
+    }
+}
+
+const excludedSelectors = ['aside', 'nav'];
+
+document.body.addEventListener('click', function (event) {
+    for (const selector of excludedSelectors) {
+        if (event.target.closest(selector)) {
+            return;
+        }
+    }
+
+    if (document.getElementById("mySidenav").style.width === "300px") {
+        closeNav();
+    }
+});
